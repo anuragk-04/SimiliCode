a9279e4e6f70b7773d7e69685a5027c121c3a0b7
anuragk-04 <anurag04kushwaha@gmail.com>
2025-07-24 02:33:48 +0530

    frontend initiated

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import HomePage from "./pages/homePage"
+import './main.css'
+
+function App() {
+
+  return (
+    <div>
+      <HomePage />
+    </div>
+  )
+}
+
+export default App
